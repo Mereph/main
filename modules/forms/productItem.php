b23ae4467d760b94ed109a03afa1ad3b658c8ce1
Fleisar <me@fle.su>
2022-02-01 00:28:26 +0700

    refactor productItem

--- modules/forms/productItem.php
+++ modules/forms/productItem.php
@@ -5,19 +5,16 @@ function productItem(object $data): string {
   $description = $data->description ?? "Нет описания";
   $price = (int) $data->price;
 return <<<HTML
-<div class="shop_product">
-  <div class="ecosystem_support_tab" style="margin-bottom: 2.2em;">
-    <h3>${name}</h3>
-    <div class="align_sphere" style="width: 25px;">
-      <div class="sphere"></div>
-    </div>
-  </div>
-  <hr>
-  <p>${description}</p>
-  <form method="POST" action="payment_state">
-    <input type="hidden" name="product_id" value="${id}">
-    <button>Купить за ${price}₽</button>
-  </form>
+<div class="productItem">
+    <h3 class="productItem__title">${name}</h3>
+    <hr>
+    <p class="productItem__description">${description}</p>
+    <form method="post" action="payment_state" class="productItem__payContainer">
+        <input type="hidden" name="product_id" value="${id}">
+        <button class="productItem__button">
+            Купить за ${price}₽
+        </button>
+    </form>
 </div>
 HTML;
 }
