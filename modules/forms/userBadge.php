5289958a478138c0f4126017126fd601627b2e55
Fleisar <me@fle.su>
2022-02-01 22:29:46 +0700

    create userBadge

---
+++ modules/forms/userBadge.php
@@ -0,0 +1,21 @@
+<?php
+
+function userBadge(object|null $user): string {
+	if ($user == null) {
+		$auth_path = route."/auth";
+		return <<< HTML
+			<a class="button_link" href="${auth_path}">
+				<button>Авторизоваться</button>
+			</a>
+		HTML;
+	}
+	$display_name = nameFormat($user);
+	$balance = $user->balance;
+	$profile_path = route."/profile";
+	return <<< HTML
+		<a class="userBadge" href="${profile_path}">
+			<span class="userBadge__name">${display_name}</span>
+			<div class="userBadge__balance">${balance}₽</div>
+		</a>
+	HTML;
+}
