7ed852ebb5eb393fe2ab02401fb2aa282cfadc31
Fleisar <me@fle.su>
2022-02-06 16:21:01 +0700

    place form into container

--- register.php
+++ register.php
@@ -9,40 +9,42 @@ require 'imports/user.php';
 	<?php require_once "modules/metaTags.php" ?>
 </head>
 <body>
-	<a class="button_link" href="<?=route."/"?>">
-		<img src="<?=route?>/assets/image/connect.webp" width="281" height="73" alt="connect_logo">
-	</a>
-	<form method="POST" action="<?=route?>/provider/user/create.php">
-		<h2>Регистрация</h2>
-		<label>
-			<span>Логин</span>
-			<input type="text" minlength="3" maxlength="24" name="login" placeholder="misha" autocomplete="nickname" required>
-		</label>
-		<div class="split_grid">
+	<div class="container">
+		<a class="button_link" href="<?=route."/"?>">
+			<img src="<?=route?>/assets/image/connect.webp" width="281" height="73" alt="connect_logo">
+		</a>
+		<form method="POST" action="<?=route?>/provider/user/create.php">
+			<h2>Регистрация</h2>
 			<label>
-				<span>Имя</span>
-				<input type="text" maxlength="32" name="firstname" placeholder="Михаил" autocomplete="given-name" required>
+				<span>Логин</span>
+				<input type="text" minlength="3" maxlength="24" name="login" placeholder="misha" autocomplete="nickname" required>
+			</label>
+			<div class="split_grid">
+				<label>
+					<span>Имя</span>
+					<input type="text" maxlength="32" name="firstname" placeholder="Михаил" autocomplete="given-name" required>
+				</label>
+				<label>
+					<span>Фамилия</span>
+					<input type="text" maxlength="32" name="lastname" placeholder="Смирнов" autocomplete="family-name" required>
+				</label>
+			</div>
+			<label>
+				<span>E-mail</span>
+				<input type="email" maxlength="64" name="email" placeholder="user@example.com" autocomplete="email" required>
 			</label>
 			<label>
-				<span>Фамилия</span>
-				<input type="text" maxlength="32" name="lastname" placeholder="Смирнов" autocomplete="family-name" required>
+				<span>Пароль</span>
+				<input type="password" name="password" maxlength="32" minlength="8" placeholder="••••••••" autocomplete="new-password" required>
 			</label>
-		</div>
-		<label>
-			<span>E-mail</span>
-			<input type="email" maxlength="64" name="email" placeholder="user@example.com" autocomplete="email" required>
-		</label>
-		<label>
-			<span>Пароль</span>
-			<input type="password" name="password" maxlength="32" minlength="8" placeholder="••••••••" autocomplete="new-password" required>
-		</label>
-		<button name="tos" value="1" class="auth_box_submit">Продолжить</button>
-		<a href="auth">Уже есть аккаунт</a>
-	</form>
-	<p>
-		Нажимая кнопку «Продолжить» вы соглашаетесь с
-		<a class="mark--main" href="<?=route?>/terms.php">Условиями использования</a>
-		сервиса.
-	</p>
+			<button name="tos" value="1">Продолжить</button>
+			<a href="<?=route?>/auth.php">Уже есть аккаунт</a>
+		</form>
+		<p>
+			Нажимая кнопку «Продолжить» вы соглашаетесь с
+			<a class="mark--main" href="<?=route?>/terms.php">Условиями использования</a>
+			сервиса.
+		</p>
+	</div>
 </body>
 </html>
